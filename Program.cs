36818123c9bf7594e32f1d5bb3e71c8f91e0634f
Malmsteg <73699667+Malmsteg@users.noreply.github.com>
2021-12-12 08:00:03 +0100

    Getting a start

--- Program.cs
+++ Program.cs
@@ -5,14 +5,53 @@ using System.Linq;
 
 namespace AdventOfCode_2021_Day12
 {
-   public static class Program
+    public static class Program
     {
         public static void Main()
         {
             string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            string file = Path.Combine(currentDirectory, @"..\..\..\input.txt");
+            string file = Path.Combine(currentDirectory, @"..\..\..\test.txt");
             string path = Path.GetFullPath(file);
             string[] text = File.ReadAllText(path).Replace("\r", "").Split("\n");
+
+            // Find all possible connections start to end.
+            // small caves (small letters) may only be
+            // visisted once. Big ones (capital letters)
+            // may be visited any number of times.
+            // Dash '-' means two caves are connected.
+
+            Dictionary<string,List<string>> connections = new();
+            for(int i = 0; i < text.Length; i++)
+            {
+                string[] temp = text[i].Split('-');
+                if(connections.ContainsKey(temp[0]))
+                {
+                    connections[temp[0]].Add(temp[1]);
+                }
+                else
+                {
+                    connections[temp[0]] = new List<string> {temp[1]};
+                }
+                if(connections.ContainsKey(temp[1]))
+                {
+                    connections[temp[1]].Add(temp[0]);
+                }
+                else
+                {
+                    connections[temp[1]] = new List<string> {temp[0]};
+                }
+            }
+            foreach(var item in connections)
+            {
+                Console.Write($"Key: {item.Key} Connects to: ");
+                foreach(var str in item.Value)
+                {
+                    Console.Write($"{str} ");
+                }
+                Console.WriteLine();
+            }
+
+
         }
     }
 }
